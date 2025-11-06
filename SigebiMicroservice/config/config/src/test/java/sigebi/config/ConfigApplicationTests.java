99936137108182811a35b654636b788df4de5877
DianaArevalo <morec9834@gmail.com>
2025-11-05 23:58:39 -0500

    eureka

---
+++ SigebiMicroservice/config/config/src/test/java/sigebi/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package sigebi.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
