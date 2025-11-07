1f56eba4e3df377a3b643a35fb7709011eb7c290
DianaArevalo <morec9834@gmail.com>
2025-11-06 19:51:11 -0500

    carpetas de config y gateway

---
+++ SigebiMicroservice/eureka/src/test/java/sigebi/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package sigebi.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.profiles.active=test")
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
