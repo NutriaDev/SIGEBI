980489d87b6cc850ec243a929e2348645fe9d3d1
DianaArevalo <morec9834@gmail.com>
2026-01-26 19:13:14 -0500

    PU - GetRolByIdNotFoundTest

---
+++ SigebiMicroservice/users/src/test/java/sigebi/users/services/GetRolByIdNotFoundTest.java
@@ -0,0 +1,41 @@
+package sigebi.users.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sigebi.users.exception.RoleNotFoundException;
+import sigebi.users.repository.RoleRepository;
+import sigebi.users.service.RoleService;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class GetRolByIdNotFoundTest {
+    @InjectMocks
+    RoleService roleService;
+
+    @Mock
+    RoleRepository roleRepository;
+
+    @Test
+    void getRoleById_notFound_throwsException() {
+
+        when(roleRepository.findById(99))
+                .thenReturn(Optional.empty());
+
+        RoleNotFoundException exception = assertThrows(
+                RoleNotFoundException.class,
+                () -> roleService.getRoleById(99)
+        );
+
+        assertEquals("Role not found with ID: 99", exception.getMessage());
+
+        verify(roleRepository).findById(99);
+    }
+
+}
