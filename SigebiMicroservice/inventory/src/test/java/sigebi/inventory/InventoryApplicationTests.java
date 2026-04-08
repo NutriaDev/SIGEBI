9bcfba2348b0885ea2f4a6aa25ab44addcde4afb
Juan Ortiz <juancortiza97@gmail.com>
2026-04-07 20:41:49 -0500

    chore (Inventory): Add resources and test the structure

---
+++ SigebiMicroservice/inventory/src/test/java/sigebi/inventory/InventoryApplicationTests.java
@@ -0,0 +1,22 @@
+package inventory.src.test.java.sigebi.inventory;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false",
+        "equipment-service.url=http://localhost:8089"
+})
+class InventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
