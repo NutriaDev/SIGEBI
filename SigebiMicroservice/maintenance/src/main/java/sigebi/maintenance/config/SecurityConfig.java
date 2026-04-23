ccd6cb937834d2314dbbaa578c5893d4d013b502
DianaArevalo <morec9834@gmail.com>
2026-04-22 22:37:29 -0500

    fixed controller with apiresponse and security

--- SigebiMicroservice/maintenance/src/main/java/sigebi/maintenance/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package sigebi.maintenance.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
-@Configuration
-@EnableMethodSecurity
-public class SecurityConfig {
-}
