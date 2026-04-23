a9bbedfc51a4d3b1bf19a5826dbcb9459716ecd8
DianaArevalo <morec9834@gmail.com>
2026-04-22 21:04:45 -0500

    refactor DTOs

--- SigebiMicroservice/maintenance/src/main/java/sigebi/maintenance/dto_request/MaintenanceRequest.java
+++ SigebiMicroservice/maintenance/src/main/java/sigebi/maintenance/dto_request/MaintenanceRequest.java
@@ -3,9 +3,15 @@ package sigebi.maintenance.dto_request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MaintenanceRequest {
 
     @NotNull(message = "El equipo es obligatorio")
@@ -25,52 +31,4 @@ public class MaintenanceRequest {
     private Long technicianId;
 
     private String replacedParts;
-
-    public Long getEquipmentId() {
-        return equipmentId;
-    }
-
-    public void setEquipmentId(Long equipmentId) {
-        this.equipmentId = equipmentId;
-    }
-
-    public Long getMaintenanceType() {
-        return maintenanceType;
-    }
-
-    public void setMaintenanceType(Long maintenanceType) {
-        this.maintenanceType = maintenanceType;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getTechnicianId() {
-        return technicianId;
-    }
-
-    public void setTechnicianId(Long technicianId) {
-        this.technicianId = technicianId;
-    }
-
-    public String getReplacedParts() {
-        return replacedParts;
-    }
-
-    public void setReplacedParts(String replacedParts) {
-        this.replacedParts = replacedParts;
-    }
 }
