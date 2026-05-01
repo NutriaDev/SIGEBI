985dba94f2970ddfa142a3cb337f2c6ce04ffd1d
DianaArevalo <morec9834@gmail.com>
2026-04-30 21:15:23 -0500

    initial commit entities

---
+++ SigebiMicroservice/reportsandaudit/src/main/java/sigebi/reportsandaudit/entities/MovementReportViewEntity.java
@@ -0,0 +1,35 @@
+package sigebi.reportsandaudit.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "movement_report_view")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MovementReportViewEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long movementId;
+
+    @Column(nullable = false)
+    private Long equipmentId;
+
+    @Column(nullable = false)
+    private Long originLocationId;
+
+    @Column(nullable = false)
+    private Long destinationLocationId;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private Long responsibleUserId;
+}
---
+++ SigebiMicroservice/reportsandaudit/src/test/java/sigebi/reportsandaudit/ReportsandauditApplicationTests.java
@@ -0,0 +1,15 @@
+package sigebi.reportsandaudit;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class ReportsandauditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
