a5d9835b392b564da8c79c3993d896d8c1581e94
DianaArevalo <morec9834@gmail.com>
2026-05-03 19:29:40 -0500

    consume eventos

--- SigebiMicroservice/reportsandaudit/src/main/java/sigebi/reportsandaudit/kafka/ReportEventConsumer.java
+++ SigebiMicroservice/reportsandaudit/src/main/java/sigebi/reportsandaudit/kafka/ReportEventConsumer.java
@@ -4,27 +4,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import sigebi.reportsandaudit.entities.ConsolidatedReportViewEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 import sigebi.reportsandaudit.repository.ConsolidatedReportViewRepository;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReportEventConsumer {
 
     private final ConsolidatedReportViewRepository repository;
+    private final ObjectMapper objectMapper; // 🔥 ESTE FALTABA
 
     @KafkaListener(
             topics = "${kafka.topics.report-events}",
             groupId = "sigebi-report-group"
     )
-    public void consume(ReportEvent event) {
+    public void consume(String message) {
+        try {
+            ReportEvent event = objectMapper.readValue(message, ReportEvent.class);
 
-        ConsolidatedReportViewEntity entity =
-                mapToEntity(event);
+            ConsolidatedReportViewEntity entity = mapToEntity(event);
 
-        repository.save(entity);
+            repository.save(entity);
+
+            log.info("Evento procesado correctamente: {}", event);
+
+        } catch (Exception e) {
+            log.error("Error procesando ReportEvent", e);
+        }
     }
 
     private ConsolidatedReportViewEntity mapToEntity(ReportEvent event) {
